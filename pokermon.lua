712fc3c2762df416cf40331ed0d60728aa74275c
InertSteak <charlesbaker8888@gmail.com>
2024-12-24 18:08:32 -0800

    Switched Rapidash to using Steamodded get_straight

--- pokermon.lua
+++ pokermon.lua
@@ -371,49 +371,87 @@ if not pokermon_config.jokers_only then
   end 
 end
 
---Override straight function for Rapidash
+--Override straight function for Rapidash, this is the steamodded version
+local straight_ref = get_straight
 function get_straight(hand)
-  local ret = {}
-  local four_fingers = next(find_joker('Four Fingers'))
-  if #hand > 5 or #hand < (5 - (four_fingers and 1 or 0)) then return ret else
+  local value = straight_ref(hand)
+  if not value or (type(value) == 'table' and #value == 0) then
+    local ret = value
+    local four_fingers = next(SMODS.find_card('j_four_fingers'))
+    local can_skip = next(SMODS.find_card('j_poke_rapidash'))
+    if #hand < (5 - (four_fingers and 1 or 0)) then return ret end
     local t = {}
-    local IDS = {}
-    for i=1, #hand do
-      local id = hand[i]:get_id()
-      if id > 1 and id < 15 then
-        if IDS[id] then
-          IDS[id][#IDS[id]+1] = hand[i]
-        else
-          IDS[id] = {hand[i]}
-        end
+    local RANKS = {}
+    for i = 1, #hand do
+      if hand[i]:get_id() > 0 then
+        local rank = hand[i].base.value
+        RANKS[rank] = RANKS[rank] or {}
+        RANKS[rank][#RANKS[rank] + 1] = hand[i]
       end
     end
-
     local straight_length = 0
     local straight = false
-    local can_skip = next(find_joker('Shortcut')) or (next(find_joker('rapidash')) and G.GAME.current_round.hands_played == 0)
     local skipped_rank = false
-    for j = 1, 14 do
-      if IDS[j == 1 and 14 or j] then
-        straight_length = straight_length + 1
-        skipped_rank = false
-        for k, v in ipairs(IDS[j == 1 and 14 or j]) do
-          t[#t+1] = v
-        end
-      elseif can_skip and not skipped_rank and j ~= 14 then
-          skipped_rank = true
-      else
-        straight_length = 0
-        skipped_rank = false
-        if not straight then t = {} end
-        if straight then break end
+    local vals = {}
+    for k, v in pairs(SMODS.Ranks) do
+      if v.straight_edge then
+        table.insert(vals, k)
       end
-      if straight_length >= (5 - (four_fingers and 1 or 0)) then straight = true end 
+    end
+    local init_vals = {}
+    for _, v in ipairs(vals) do
+      init_vals[v] = true
+    end
+    if not next(vals) then table.insert(vals, 'Ace') end
+    local initial = true
+    local br = false
+    local end_iter = false
+    local i = 0
+    while 1 do
+      end_iter = false
+      if straight_length >= (5 - (four_fingers and 1 or 0)) then
+        straight = true
+      end
+      i = i + 1
+      if br or (i > #SMODS.Rank.obj_buffer + 1) then break end
+      if not next(vals) then break end
+      for _, val in ipairs(vals) do
+        if init_vals[val] and not initial then br = true end
+        if RANKS[val] then
+          straight_length = straight_length + 1
+          skipped_rank = false
+          for _, vv in ipairs(RANKS[val]) do
+            t[#t + 1] = vv
+          end
+          vals = SMODS.Ranks[val].next
+          initial = false
+          end_iter = true
+          break
+        end
+      end
+      if not end_iter then
+        local new_vals = {}
+        for _, val in ipairs(vals) do
+          for _, r in ipairs(SMODS.Ranks[val].next) do
+            table.insert(new_vals, r)
+          end
+        end
+        vals = new_vals
+        if can_skip and not skipped_rank then
+          skipped_rank = true
+        else
+          straight_length = 0
+          skipped_rank = false
+          if not straight then t = {} end
+          if straight then break end
+        end
+      end
     end
     if not straight then return ret end
     table.insert(ret, t)
     return ret
   end
+  return value
 end
 
 local set_edition = Card.set_edition
