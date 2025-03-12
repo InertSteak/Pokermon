2a486a2d7a96745b50549ba1d4fe2d3ad2d09e53
InertSteak <charlesbaker8888@gmail.com>
2025-03-11 17:37:51 -0700

    Zorua legendary fix

--- pokemon/pokejokers_19.lua
+++ pokemon/pokejokers_19.lua
@@ -135,7 +135,7 @@ local zorua = {
     if G.STAGE == G.STAGES.RUN and card.area == G.jokers then
       local other_joker = G.jokers.cards[#G.jokers.cards]
       card.ability.blueprint_compat = ( other_joker and other_joker ~= card and not other_joker.debuff and other_joker.config.center.blueprint_compat and 'compatible') or 'incompatible'
-      if card.ability.blueprint_compat == 'compatible' and not card.debuff then
+      if card.ability.blueprint_compat == 'compatible' and not card.debuff and other_joker.config.center.rarity ~= 4 then
         card.children.center.atlas = other_joker.children.center.atlas
         card.children.center:set_sprite_pos(other_joker.children.center.sprite_pos)
       else
