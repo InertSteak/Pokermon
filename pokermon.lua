4cc930c46f55b441e28e56c153d0ecc7cc4557b8
InertSteak <charlesbaker8888@gmail.com>
2025-03-18 16:19:57 -0700

    Fixed Joker Display attempting to load when not present

--- pokermon.lua
+++ pokermon.lua
@@ -31,16 +31,18 @@ SMODS.UndiscoveredSprite({
 mod_dir = ''..SMODS.current_mod.path
 pokermon_config = SMODS.current_mod.config
 
+--Load Joker Display if the mod is enabled
+if (SMODS.Mods["JokerDisplay"] or {}).can_load then
+  local jokerdisplays = NFS.getDirectoryItems(mod_dir.."jokerdisplay")
 
-local jokerdisplays = NFS.getDirectoryItems(mod_dir.."jokerdisplay")
-
-for _, file in ipairs(jokerdisplays) do
-  sendDebugMessage ("The file is: "..file)
-  local helper, load_error = SMODS.load_file("jokerdisplay/"..file)
-  if load_error then
-    sendDebugMessage ("The error is: "..load_error)
-  else
-    helper()
+  for _, file in ipairs(jokerdisplays) do
+    sendDebugMessage ("The file is: "..file)
+    local helper, load_error = SMODS.load_file("jokerdisplay/"..file)
+    if load_error then
+      sendDebugMessage ("The error is: "..load_error)
+    else
+      helper()
+    end
   end
 end
 
