6082248df71ac9bb120f6db8bdb018fac4a672b2
random-facades <random.facades@gmail.com>
2025-04-08 19:43:01 -0400

    Refactor Tangrowth/Tentacruel to Debuff Functions

--- functions/debufffunctions.lua
+++ functions/debufffunctions.lua
@@ -5,19 +5,15 @@ SMODS.current_mod.set_debuff = function(card)
    if card and card.ability and card.ability.fainted == G.GAME.round then
       return G.STATE == G.STATES.SELECTING_HAND or G.STATE == G.STATES.HAND_PLAYED or G.STATE == G.STATES.DRAW_TO_HAND
    end
+   if card.ability.name == "gholdengo" then return 'prevent_debuff' end
+   if card.ability.name == "Wild Card" and next(find_joker("tangrowth")) then return 'prevent_debuff' end
+   if card:get_id() == 10 and next(find_joker("tentacruel")) then return 'prevent_debuff' end
    return false
 end
 
 
 local previous_set_debuff = Card.set_debuff
 Card.set_debuff = function(card, should_debuff)
-   if card.ability.name == "gholdengo" then
-      if card.debuff then
-         card.debuff = false
-         if card.area == G.jokers then card:add_to_deck(true) end
-      end
-      card.debuffed_by_blind = false
-   end
    previous_set_debuff(card, should_debuff)
 
    -- if Gliscor, then cancel debuff
