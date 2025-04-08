b51d17c7d48db09f09e3a9cc5eba3ed8d3d35b99
random-facades <random.facades@gmail.com>
2025-04-08 19:38:15 -0400

    Move Gholdengo debuff logic into Debuff Functions

--- functions/debufffunctions.lua
+++ functions/debufffunctions.lua
@@ -1,8 +1,24 @@
+-- Describe all the logic for debuffing or undebuffing
 
-
+-- return values: true, false, or 'prevent_debuff'
 SMODS.current_mod.set_debuff = function(card)
    if card and card.ability and card.ability.fainted == G.GAME.round then
-     return G.STATE == G.STATES.SELECTING_HAND or G.STATE == G.STATES.HAND_PLAYED or G.STATE == G.STATES.DRAW_TO_HAND
+      return G.STATE == G.STATES.SELECTING_HAND or G.STATE == G.STATES.HAND_PLAYED or G.STATE == G.STATES.DRAW_TO_HAND
    end
    return false
- end
+end
+
+
+local previous_set_debuff = Card.set_debuff
+Card.set_debuff = function(card, should_debuff)
+   if card.ability.name == "gholdengo" then
+      if card.debuff then
+         card.debuff = false
+         if card.area == G.jokers then card:add_to_deck(true) end
+      end
+      card.debuffed_by_blind = false
+   end
+   previous_set_debuff(card, should_debuff)
+
+   -- if Gliscor, then cancel debuff
+end
