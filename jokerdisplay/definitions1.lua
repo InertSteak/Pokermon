ff270021eba7064482eee96f8b068f249ea3f79d
Definite-Internet-User <72332722+Definite-Internet-User@users.noreply.github.com>
2025-05-17 20:50:11 -0400

    Improve Aerodactyl Joker Display definition

--- jokerdisplay/definitions1.lua
+++ jokerdisplay/definitions1.lua
@@ -2896,29 +2896,37 @@ end
 }
 -- Aerodactyl
 jd_def["j_poke_aerodactyl"] = {
-    text = {
-        {
-            border_nodes = {
-                { text = "X" },
-                { ref_table = "card.joker_display_values", ref_value = "Xmult", retrigger_type = "exp" },
-            },
-        },
+  text = {
+    {
+      border_nodes = {
+        { text = "X" },
+        { ref_table = "card.joker_display_values", ref_value = "Xmult", retrigger_type = "exp" },
+      },
     },
-    reminder_text = {
-        { ref_table = "card.joker_display_values", ref_value = "localized_text" }
-},
-    calc_function = function(card)
-        local Xmult = 1
-        local count = 0
-        local text, _, scoring_hand = JokerDisplay.evaluate_hand()
-        if text ~= 'Unknown' then
-            for _, scoring_card in pairs(scoring_hand) do
-                if scoring_card:get_id() == 14 then 
-                    count = count + 1
-                end
+  },
+  reminder_text = {
+    { ref_table = "card.joker_display_values", ref_value = "localized_text" }
+  },
+  calc_function = function(card)
+    local Xmult = 1
+    local count = 0
+    local text, _, scoring_hand = JokerDisplay.evaluate_hand()
+    if text ~= 'Unknown' then
+        for _, scoring_card in pairs(scoring_hand) do
+            if scoring_card:get_id() == 14 then 
+                count = count + 1
             end
         end
-        if count >= 1 then card.joker_display_values.Xmult = card.ability.extra.Xmult else card.joker_display_values.Xmult = Xmult end
+    end
+    if count >= 4 then
+      card.joker_display_values.Xmult = (card.ability.extra.Xmult_original + card.ability.extra.Xmult_mod) * 2
+    elseif count < 4 and count >= 2 then
+      card.joker_display_values.Xmult = (card.ability.extra.Xmult_original + card.ability.extra.Xmult_mod)
+    elseif count == 1 then 
+      card.joker_display_values.Xmult = card.ability.extra.Xmult_original
+    else
+      card.joker_display_values.Xmult = Xmult
+    end
     card.joker_display_values.localized_text = "(" .. localize("Ace", "ranks")..")"
     end
 }
