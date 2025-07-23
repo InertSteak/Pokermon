c8d090a1709f614bffb84068c924bdec855f8be8
InertSteak <charlesbaker8888@gmail.com>
2025-07-22 19:36:32 -0700

    Updated sprite load function to handle multiple different sprites

--- functions/pokespriteload.lua
+++ functions/pokespriteload.lua
@@ -6,6 +6,7 @@ PokemonSprites = {
   mew       = {normal = {pos = {x = 12, y = 11}, soul_pos = {x = 0, y = 12}}, alt = {pos = {x = 11, y = 12}, soul_pos = {x = 0, y = 12}}},
   missingno = {normal = {pos = {x = 1, y = 12}, soul_pos = {x = 2, y = 12}}, alt = {pos = {x = 12, y = 12}, soul_pos = {x = 2, y = 12}}},
   entei     = {normal = {pos = {x = 3, y = 9}, soul_pos = {x = 4, y = 9}}, alt = {pos = {x = 8, y = 10}, soul_pos = {x = 4, y = 9}}},
+  pyroar    = {normal = { pos = {{x = 4, y = 1}, {x= 0, y = 7}} } },
 } 
 
 poke_load_sprites = function(item)
@@ -13,8 +14,11 @@ poke_load_sprites = function(item)
   local sprite = nil
   if sprite_info then
     sprite = pokermon_config.pokemon_altart and sprite_info.alt or sprite_info.normal
+
+    local position = sprite.pos.x and sprite.pos or sprite.pos[math.random(#sprite.pos)]
+    local soul_position = sprite.soul_pos
     
-    if sprite.pos then item.pos = sprite.pos end
-    if sprite.soul_pos then item.soul_pos = sprite.soul_pos end
+    if position then item.pos = position end
+    if soul_position then item.soul_pos = soul_position end
   end
 end
