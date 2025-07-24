d99cfb921501c1850ef95dc4d42386a863606674
Definite-Internet-User <72332722+Definite-Internet-User@users.noreply.github.com>
2025-07-24 15:11:14 -0400

    JokerDisplay - Add Mega Scizor

--- jokerdisplay/definitions2.lua
+++ jokerdisplay/definitions2.lua
@@ -1202,7 +1202,32 @@ jd_def["j_poke_scizor"] = {
             },
         },
     },
+}
 
+--	Mega Scizor
+jd_def["j_poke_mega_scizor"] = {
+  reminder_text = {
+    { text = "(" },
+    { ref_table = "card.joker_display_values", ref_value = "count", colour = G.C.ORANGE },
+    { text = "x" },
+    { ref_table = "card.joker_display_values", ref_value = "localized_text", colour = G.C.BLUE },
+    { text = ")" },
+  },
+  calc_function = function(card)
+    local count = 0
+    if G.jokers then
+      for _, joker_card in ipairs(G.jokers.cards) do
+        if joker_card.config.center.rarity and joker_card.config.center.rarity == 1 then
+          count = count + 1
+        end
+      end
+    end
+    card.joker_display_values.count = count
+    card.joker_display_values.localized_text = localize("k_common")
+  end,
+  mod_function = function(card, mod_joker)
+    return { x_mult = (card.config.center.rarity == 1 and mod_joker.ability.extra.Xmult_multi ^ JokerDisplay.calculate_joker_triggers(mod_joker) or nil) }
+  end
 }
 
 --	Shuckle
