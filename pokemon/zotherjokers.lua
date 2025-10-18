bb82a87041138bc030c9c19a1e777ec2d619c095
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-18 05:06:45 +0200

    added the `skip_joker_type_destroyed` context to pokermon joker destruction effects

--- pokemon/zotherjokers.lua
+++ pokemon/zotherjokers.lua
@@ -171,17 +171,7 @@ local jelly_donut={
       if card.ability.extra.rounds <= 0 then 
         G.E_MANAGER:add_event(Event({
             func = function()
-                play_sound('tarot1')
-                card.T.r = -0.2
-                card:juice_up(0.3, 0.4)
-                card.states.drag.is = true
-                card.children.center.pinch.x = true
-                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                    func = function()
-                            G.jokers:remove_card(card)
-                            card:remove()
-                            card = nil
-                        return true; end})) 
+                remove(self, card, context)
                 return true
             end
         })) 
@@ -224,17 +214,7 @@ local treasure_eatery={
       if card.ability.extra.rounds <= 0 then 
         G.E_MANAGER:add_event(Event({
             func = function()
-                play_sound('tarot1')
-                card.T.r = -0.2
-                card:juice_up(0.3, 0.4)
-                card.states.drag.is = true
-                card.children.center.pinch.x = true
-                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                    func = function()
-                            G.jokers:remove_card(card)
-                            card:remove()
-                            card = nil
-                        return true; end})) 
+                remove(self, card, context)
                 return true
             end
         })) 
