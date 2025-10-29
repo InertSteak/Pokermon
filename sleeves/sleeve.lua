46a607c49698a1463197c0f34f46a4ef23a290d5
Catzzadilla <flixrob@gmail.com>
2025-10-28 23:22:41 -0300

    Added Vending Combo
    you know the drill, no localization. This one is simple: when the sleeve is paired with the deck, it also gives a voucher tag after every even blind.

--- sleeves/sleeve.lua
+++ sleeves/sleeve.lua
@@ -216,6 +216,18 @@ local vendingsleeve = {
 	pos = { x = 7, y = 1 },
 	atlas = "AtlasDecksBasic",
     calculate = function(self, back, context)
+     if self.get_current_deck_key() == "b_poke_vendingdeck" then
+      if context.round_eval and G.GAME.last_blind and G.GAME.last_blind.boss and ((G.GAME.round_resets.ante) % 2 == 1) then
+          G.E_MANAGER:add_event(Event({
+              func = function()
+                  add_tag(Tag('tag_voucher'))
+                  play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+                  play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
+                  return true
+              end
+          }))
+      end
+    else
       if context.round_eval and G.GAME.last_blind and G.GAME.last_blind.boss and ((G.GAME.round_resets.ante - 1) % 2 == 1) then
           G.E_MANAGER:add_event(Event({
               func = function()
@@ -226,7 +238,8 @@ local vendingsleeve = {
               end
           }))
       end
-    end,
+    end
+  end
 }
 
 local slist = nil
