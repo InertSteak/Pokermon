f25a57fc1fdc27f7dce0e8c63519a12049e65353
Emma Vedel <emmavedelpedersen@gmail.com>
2025-10-30 21:39:50 +0100

    made artist page sort ignore casing

--- functions/pokespriteload.lua
+++ functions/pokespriteload.lua
@@ -1214,6 +1214,6 @@ poke_get_artist_list = function()
   for artist, _ in pairs(poke_artist_info) do
     list[#list+1] = artist
   end
-  table.sort(list)
+  table.sort(list, function(a,b) return a:lower() < b:lower() end)
   return list
 end
