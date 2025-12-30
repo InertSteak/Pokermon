541bf3a05400563090c70c921285cf36288d1275
Eternalnacho <ahanshawbhaskar@gmail.com>
2025-12-30 16:23:09 -0600

    null_checking card in is_type

--- functions/pokefunctions.lua
+++ functions/pokefunctions.lua
@@ -242,7 +242,7 @@ find_pokemon_type = function(target_type, exclude_card)
 end
 
 is_type = function(card, target_type)
-  if get_type(card) == target_type or card.ability[string.lower(target_type).."_sticker"] then
+  if card and get_type(card) == target_type or card.ability[string.lower(target_type).."_sticker"] then
     return true
   else
     return false
