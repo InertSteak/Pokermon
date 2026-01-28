4bc466b6de2fa12f361f205ce55e5d9693a208ef
InertSteak <charlesbaker8888@gmail.com>
2026-01-28 09:15:20 -0800

    Updated challenge rule description

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -6985,7 +6985,7 @@ return {
            ch_c_poke_add_joker_slots = {"{C:attention}+1{} Joker Slot when Boss Blind defeated {C:inactive}(up to 5)"},
            ch_c_poke_nuzlocke = {"First shop of each Ante contains a {C:attention}Buffoon Pack"},
            ch_c_apply_randomizer = {"Pokemon Jokers evolve into random Pokemon Jokers"},
-           ch_c_no_energy = {"Energy no longer appears in the {C:attention}shop"},
+           ch_c_no_energy = {"Energy cards no longer appear in the {C:attention}shop"},
         },
     }
 }
