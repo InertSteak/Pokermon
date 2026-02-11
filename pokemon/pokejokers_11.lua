7663d872d3d314dad7b7f97fb134675185604cb0
Emma Vedel <emmavedelpedersen@gmail.com>
2026-02-11 14:57:59 +0100

    cleanup mega camerupt

--- pokemon/pokejokers_11.lua
+++ pokemon/pokejokers_11.lua
@@ -414,13 +414,13 @@ local mega_camerupt={
   name = "mega_camerupt",
   pos = {x = 6, y = 4},
   soul_pos = {x = 7, y = 4},
-  config = {extra = {Xmult = 1, Xmult2 = 1, Xmult_multi = 0.5}},
+  config = {extra = {Xmult = 1, Xmult2 = 1, Xmult_mod = 0.5}},
   loc_vars = function(self, info_queue, center)
     type_tooltip(self, info_queue, center)
     if pokermon_config.detailed_tooltips then
       info_queue[#info_queue+1] = G.P_CENTERS.m_mult
     end
-    return {vars = {center.ability.extra.Xmult, center.ability.extra.Xmult_multi}}
+    return {vars = {center.ability.extra.Xmult, center.ability.extra.Xmult_mod}}
   end,
   designer = "xtremepolymath",
   rarity = "poke_mega",
@@ -433,22 +433,21 @@ local mega_camerupt={
   blueprint_compat = true,
   eternal_compat = true,
   calculate = function(self, card, context)
-    if context.cardarea == G.jokers and context.scoring_hand then
-      if context.joker_main and card.ability.extra.Xmult > 1 then
-        return {
-          message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.Xmult}}, 
-          colour = G.C.XMULT,
-          Xmult_mod = card.ability.extra.Xmult
-        }
-      end
+    if context.joker_main then
+      return {
+        Xmult = card.ability.extra.Xmult
+      }
     end
-    if context.individual and not context.end_of_round and context.cardarea == G.play and not context.blueprint and SMODS.has_enhancement(context.other_card, 'm_mult') then
-      card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_multi
-      card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize("k_upgrade_ex"), colour = G.C.XMULT})
+    if context.individual and context.cardarea == G.play and not context.blueprint and SMODS.has_enhancement(context.other_card, 'm_mult') then
+      SMODS.scale_card(card, {
+        ref_value = 'Xmult',
+        scalar_value = 'Xmult_mod',
+        message_colour = G.C.XMULT
+      })
     end
     if context.end_of_round and not context.individual and not context.repetition then
       card.ability.extra.Xmult = card.ability.extra.Xmult2
-      
+
       return {
         message = localize('k_reset'),
         colour = G.C.RED
