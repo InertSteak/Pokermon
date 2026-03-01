3a0cf650c7970ddeabd5a7cb6a9de7c5e939a617
Emma Vedel <emmavedelpedersen@gmail.com>
2026-03-01 18:59:44 +0100

    change `get_random_poke_key` to use `G.P_CENTER_POOLS.Joker` instead of `G.P_CENTERS`

--- functions/pokefunctions.lua
+++ functions/pokefunctions.lua
@@ -851,7 +851,7 @@ get_random_poke_key = function(pseed, stage, pokerarity, _area, poketype, exclud
   local valid_stages = poke_convert_to_set(stage)
   local valid_rarities = poke_convert_to_set(pokerarity)
 
-  for k, v in pairs(G.P_CENTERS) do
+  for k, v in pairs(G.P_CENTER_POOLS.Joker) do
     if v.stage and v.stage ~= "Other" and (not valid_stages or valid_stages[v.stage]) and (not valid_rarities or valid_rarities[v.rarity]) and get_gen_allowed(v)
        and not (poketype and poketype ~= v.ptype) and not poke_family_present(v) and (not (type(v.in_pool) == 'function') or v:in_pool()) and not v.aux_poke and v.rarity ~= "poke_mega" and not exclude_keys[v.key]
        and not G.GAME.banned_keys[v.key] and not (G.GAME.used_jokers[v.key] and not SMODS.showman(v.key)) then
