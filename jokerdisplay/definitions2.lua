d56e754fe9163f470ea10cf0bcf571f260ed5ab4
Definite-Internet-User <72332722+Definite-Internet-User@users.noreply.github.com>
2026-06-13 14:07:12 -0400

    JokerDisplay - Commenting out Onix and Steelix

--- jokerdisplay/definitions2.lua
+++ jokerdisplay/definitions2.lua
@@ -1061,16 +1061,17 @@ jd_def["j_poke_gligar"] = {
 }
 
 --	Steelix
-jd_def["j_poke_steelix"] = {
-    text = {
-        {ref_table ="card.joker_display_values", ref_value = "status", colour = G.C.GREY}
-    },
-    calc_function = function(card)
-        local status = "Not Active!"
-        if G.GAME.current_round.hands_played == 0 then status = "Active!" end
-        card.joker_display_values.status = status
-    end
-}
+-- jd_def["j_poke_steelix"] = {
+--     text = {
+--         {ref_table ="card.joker_display_values", ref_value = "status", colour = G.C.GREY}
+--     },
+--     calc_function = function(card)
+--         local status = "Not Active!"
+--         if G.GAME.current_round.hands_played == 0 then status = "Active!" end
+--         card.joker_display_values.status = status
+--     end
+-- }
+
 --[[
 jd_def["j_poke_mega_steelix"] = {
     text = {
