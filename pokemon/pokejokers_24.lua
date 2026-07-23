b5a33f72131aafbf47628d7de4f0e05995ebb1ac
Eternalnacho <ahanshawbhaskar@gmail.com>
2026-07-22 22:19:45 -0500

    pumpkaboo discard counter fix

--- pokemon/pokejokers_24.lua
+++ pokemon/pokejokers_24.lua
@@ -100,12 +100,12 @@ local pumpkaboo = {
   calculate = function(self, card, context)
     if context.pre_discard and not context.blueprint then
       local jacks = pokermon.filter(context.full_hand, function(v) return v:get_id() == 11 and not v.debuff end)
-      local discarded, target = card.ability.extra.jacks_discarded, card.ability.extra.jack_target
+      local target = card.ability.extra.jack_target
       for _, v in ipairs(jacks) do
-        discarded = discarded + 1
-        if discarded == target then
+        card.ability.extra.jacks_discarded = card.ability.extra.jacks_discarded + 1
+        if card.ability.extra.jacks_discarded == target then
           v['pumpkaboo_trigger'..card.unique_val] = true
-          discarded = 0
+          card.ability.extra.jacks_discarded = 0
         end
       end
     end
